b5cfa9a02852dc2ecafd77d7559b6cd3fe0c2b4d
Castleexe <108441555+Castleexe@users.noreply.github.com>
2024-10-10 20:24:01 -0700

    fixed bugs

--- Learning Pygame/main.py
+++ Learning Pygame/main.py
@@ -6,14 +6,14 @@ from os import listdir
 from os.path import isfile, join #importing these to load sprites dynamically
 pygame.init()
 
-pygame.display.setcaption("Platformer") #setting window name
+pygame.display.set_caption("Platformer") #setting window name
 
-BGCOLOR = (255, 255, 255)
+BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
-PLAYERVEL = 5 #player speed
+PLAYER_VEL = 5 #player speed
 
-window = pygame.display.setmode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main(window):
     clock = pygame.time.Clock()
@@ -21,7 +21,7 @@ def main(window):
     run = True
     while(run):
         clock.tick(FPS)
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -29,5 +29,5 @@ def main(window):
     pygame.quit()
     quit()
 
-if __name == "__main":
-    main(window) #this line makes it so that only call the main func only when we call it directly
+if __name__ == "__main__":
+    main(window) #this line makes it so that only call the main func only when we call it directly 
