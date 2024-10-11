ccec4d006517d434e0038208cd617026571f6269
Castleexe <108441555+Castleexe@users.noreply.github.com>
2024-10-10 20:38:45 -0700

    added background

--- Learning Pygame/main.py
+++ Learning Pygame/main.py
@@ -8,15 +8,32 @@ pygame.init()
 
 pygame.display.set_caption("Platformer") #setting window name
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5 #player speed
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _,_, width, height = image.get_rect() #_ for values we dont care about
+    tiles = []
+
+    for i in range(WIDTH // width + 1): #tells x amount tiles
+        for j in range(HEIGHT // height + 1):  #tells y amount tiles
+            pos = (i * width, j * height) #pos of top left tile of current tile
+            tiles.append(pos)
+    return tiles, image
+
+def draw(window, background, bg_img):
+    for tile in background:
+        window.blit(bg_img, tile)
+
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_img = get_background("Green.png")
 
     run = True
     while(run):
@@ -26,6 +43,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_img)
     pygame.quit()
     quit()
 
