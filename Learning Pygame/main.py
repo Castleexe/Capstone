a94a9d6b5252acded33d0ce1a256420837e29738
Castleexe <108441555+Castleexe@users.noreply.github.com>
2024-10-17 18:14:46 -0700

    done animating the player

--- Learning Pygame/main.py
+++ Learning Pygame/main.py
@@ -46,6 +46,7 @@ class Player(pygame.sprite.Sprite):
     color = (255, 0, 0)
     GRAVITY = 1 #acceleration
     SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+    ANIMATION_DEALY = 3
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -77,9 +78,25 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+        
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DEALY) % len(sprites) #updates sprite index
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update():
+        self.rect = self.sprite.get_rect(topLeft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite) #tells us where the pixels are
 
     def draw(self, win):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
