060500bc12ed1f98f2795ef6cf6cd9e0fc2f3a83
Castleexe <108441555+Castleexe@users.noreply.github.com>
2024-10-25 14:44:24 -0700

    added horizontal collision

--- Learning Pygame/main.py
+++ Learning Pygame/main.py
@@ -190,13 +190,30 @@ def handle_vertical_collision(player, objects, dy):
     
     return collided_objects
 
+def colldie(player, objects, dx):
+    player.move(dx, 0)
+    player.update() #moving player where they would be if they moved horizontally
+    collided_object = None
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            collided_object = obj
+            break
+
+    player.move(-dx, 0)
+    player.update()
+    return collided_object
+
+
 def handle_move(player, objects):
     keys = pygame.key.get_pressed()
     
     player.x_vel = 0
-    if keys[pygame.K_a]:
-        player.move_left(PLAYER_VEL)
-    if keys[pygame.K_d]:
+    colldie_left = colldie(player, objects, -PLAYER_VEL * 2)
+    colldie_right = colldie(player, objects, PLAYER_VEL * 2)
+
+    if keys[pygame.K_a] and not colldie_left:
+        player.move_left(PLAYER_VEL) 
+    if keys[pygame.K_d] and not colldie_right:
         player.move_right(PLAYER_VEL) 
 
     handle_vertical_collision(player, objects, player.y_vel)
@@ -209,6 +226,8 @@ def main(window):
 
     player = Player(100,100,50,50)
     floor = [Block(i * block_size, HEIGHT-block_size, block_size) for i in range(-WIDTH // block_size, (WIDTH*2) // block_size)]
+    objects = [*floor, Block(0, HEIGHT- block_size * 2, block_size), 
+    Block(block_size * 3, HEIGHT- block_size * 4, block_size)]
 
     offset_x = 0
     scroll_area_width = 200
@@ -227,8 +246,8 @@ def main(window):
                     player.jump()
 
         player.loop(FPS)
-        handle_move(player, floor)
-        draw(window, background, bg_img, player, floor, offset_x)
+        handle_move(player, objects)
+        draw(window, background, bg_img, player, objects, offset_x)
 
         if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or (
                 (player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
